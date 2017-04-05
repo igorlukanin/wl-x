a532fbd103d1e4a4e4d0e0b291beb372d02780fa
Igor Lukanin <mail@igor.lukanin.name>
2017-04-06 04:06:37 +0500

    Wunderlist brand elements (font & colors), emoji support in list titles, better layout, sorting and look in general

--- src/app/website.js
+++ src/app/website.js
@@ -17,6 +17,8 @@ express()
 
     .use('/', express.static('static'))
     .use('/moment.js', express.static('node_modules/moment/min/moment.min.js'))
+    .use('/fonts', express.static('node_modules/lato-font/fonts'))
+    .use('/fonts/lato.css', express.static('node_modules/lato-font/css/lato-font.min.css'))
 
     .get('/', (req, res) => users.getByToken(req)
         .then(user => res.render('user', { user }))
--- static/data.js
+++ static/data.js
@@ -1,40 +1,128 @@
 (function() {
-    var drawTask = function(data) {
-        var task = d3.select(this)
+    var drawSubtask = function(data) {
+        var subtask = d3.select(this)
             .append('div')
             .classed('days__day__tasks__task', true);
 
-        task
+        subtask.append('div')
+            .classed('days__day__tasks__task__title', true)
+            .text(data.title);
+    };
+
+    var emojiRegex = /^([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/;
+
+    var getListTitle = function(list) {
+        var emoji = list.title.match(emojiRegex);
+
+        return emoji === null
+            ? list.title
+            : list.title.replace(emoji[0], emoji[0] + ' ');
+    };
+
+    let usedLists = [];
+
+    var drawTask = function(data) {
+        var task = d3.select(this)
             .append('div')
-            .classed('days__day__tasks__task__list', true)
-            .text(data.list.title);
+            .classed('days__day__tasks__task', true)
+            .classed('days__day__tasks__task_not-completed', !data.task.completed);
+
+        if (usedLists.indexOf(data.list.id) === -1) {
+            usedLists.push(data.list.id);
+
+            task.append('div')
+                .classed('days__day__tasks__task__list', true)
+                .html(getListTitle(data.list));
+        }
 
         task.append('div')
             .classed('days__day__tasks__task__title', true)
             .text(data.task.title);
 
-        console.log(data);
+        if (data.task.subtasks.length > 0) {
+            task.append('div')
+                .classed('days__day__tasks__task__subtasks', true)
+                .selectAll('*')
+                .data(data.task.subtasks)
+                .enter()
+                .each(drawSubtask);
+        }
+    };
+
+    var getRelevantSubtasks = function(day, task) {
+        var today = moment(day.start);
+        var tomorrow = moment(day.end);
+
+        return task.task.subtasks
+            .filter(subtask => subtask.completed
+                && moment(subtask.completed_at).isAfter(today)
+                && moment(subtask.completed_at).isBefore(tomorrow))
+            .sort(function(a, b) {
+                return moment(a.completed_at).unix() - moment(b.completed_at).unix();
+            });
     };
 
     var drawDay = function(data) {
+        var today = moment(data.day.start);
+
         var day = d3.select(this)
             .append('div')
-            .classed('days__day', true);
+            .classed('days__day', true)
+            .classed('days__day_weekend', today.isoWeekday() >= 6);
 
         day.append('div')
             .classed('days__day__title', true)
-            .text(moment(data.day.start).format('dddd'));
+            .text(today.format('dddd'));
 
-        day.append('div')
-            .classed('days__day__date', true)
-            .text(moment(data.day.start).format('MMM D'));
+        usedLists = [];
 
-        day.append('div')
-            .classed('days__day__tasks', true)
-            .selectAll('*')
-            .data(data.tasks)
-            .enter()
-            .each(drawTask);
+        if (data.tasks.length > 0) {
+            data.tasks.forEach(task => {
+                task.task.subtasks = getRelevantSubtasks(data.day, task);
+            });
+
+            day.append('div')
+                .classed('days__day__tasks', true)
+                .selectAll('*')
+                .data(data.tasks)
+                .enter()
+                .each(drawTask);
+        }
+        else {
+            day.append('div')
+                .classed('days__day__tasks-placeholder', true)
+                .text('No tasks yet');
+        }
+    };
+
+    var getWeekName = function(days) {
+        return 'Week ' + moment(days[0].day.start).isoWeek();
+    };
+
+    var getWeekDates = function(days) {
+        var firstDay = moment(days[0].day.start);
+        var lastDay = moment(days[days.length - 1].day.start);
+
+        return firstDay.month() === lastDay.month()
+            ? firstDay.format('MMM D') + '–' + lastDay.format('D')
+            : firstDay.format('MMM D') + '&thinsp;–&thinsp;' + lastDay.format('MMM D');
+    };
+
+    var getWeekSummary = function(days) {
+        var tasks = days.reduce((count, day) => {
+            return count + day.tasks
+                .filter(task => task.task.completed).length;
+        }, 0);
+
+        var subtasks = days.reduce((count, day) => {
+            return count + day.tasks
+                .filter(task => !task.task.completed)
+                .reduce((subcount, task) => {
+                    return subcount + getRelevantSubtasks(day.day, task).length;
+                }, 0);
+        }, 0);
+
+        return { tasks, subtasks };
     };
 
     var drawDays = function(days) {
@@ -44,10 +132,28 @@
             .enter()
             .each(drawDay);
 
-        d3.select('.days')
+        var summary = d3.select('.days')
             .append('div')
             .classed('days__day', true)
             .classed('days__day_summary', true);
+
+        summary.append('div')
+            .classed('days__day__title', true)
+            .text(getWeekName(days));
+
+        summary.append('div')
+            .classed('days__day__date', true)
+            .html(getWeekDates(days));
+
+        var counts = getWeekSummary(days);
+
+        summary.append('div')
+            .classed('days__day__week-summary', true)
+            .html(counts.tasks + ' tasks');
+
+        summary.append('div')
+            .classed('days__day__week-summary__subtasks', true)
+            .html(counts.subtasks + ' subtasks');
     };
 
     d3.json('/tasks.json', drawDays);
