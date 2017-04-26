542b624a3ef298ad78d6cdb20ed1eaed368ac138
Igor Lukanin <mail@igor.lukanin.name>
2017-04-26 14:14:11 +0500

    Better plurals

--- static/data.js
+++ static/data.js
@@ -208,11 +208,11 @@
 
         summary.append('div')
             .classed('days__day__week-summary', true)
-            .html(counts.tasks + ' tasks');
+            .html(counts.tasks + (counts.tasks == 1 ? ' tasks' : ' task'));
 
         summary.append('div')
             .classed('days__day__week-summary__subtasks', true)
-            .html(counts.subtasks + ' subtasks');
+            .html(counts.subtasks + (counts.subtasks == 1 ? ' subtasks' : ' subtask'));
     };
 
     const dummyDays = [
